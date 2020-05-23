7682e5dc9b86fcfff2d60d8a2d3ff85a9f0bcf4a
Shubham Singh <shubham37@outlook.com>
2020-05-23 17:09:33 +0530

    API connection established

---
+++ apii.py
@@ -0,0 +1,27 @@
+import requests
+URl = "https://api.publicapis.org"
+TOKEN_URL = "https://public-apis-api.herokuapp.com/api/v1/auth/token"
+URL = "https://public-apis-api.herokuapp.com/api/v1/apis/categories?page=1"
+
+def get_token ():
+    url = "https://public-apis-api.herokuapp.com/api/v1/auth/token"
+    payload = {}
+    headers = {}
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    return(response.json())
+#data = get_token()
+#print(data.get('token'))
+def get_list():
+    url = "https://public-apis-api.herokuapp.com/api/v1/apis/categories?page=1"
+    token = f"Bearer {get_token().get('token')}"
+    print(token)
+    payload = {}
+    headers = {"Authorization": token}
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    print(response.text.encode('utf8'))
+
+get_list()
+
